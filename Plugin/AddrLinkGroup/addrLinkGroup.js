d4d6ccc42343bbafc20a7cc3ddd0ccc2c6151660
planet <weiboxin100@gmail.comm>
2026-05-28 10:07:08 +0800

    添加收藏链接读取逻辑

--- Plugin/AddrLinkGroup/addrLinkGroup.js
+++ Plugin/AddrLinkGroup/addrLinkGroup.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    var added = localStorage.getItem('addedWebAddress');
+    var selected = localStorage.getItem('selectededWebAddress');
+    var links = new Map();
+    if (selected) new Map(JSON.parse(selected)).forEach(function(v, k) { links.set(k, v); });
+    if (added) new Map(JSON.parse(added)).forEach(function(v, k) { links.set(k, v); });
+
+    links.forEach(function(name, url) {
+        $('#addr-link-row').append(
+            '<a class="addr_link" href="' + url + '" target="_blank">' +
+                '<span class="link_name">' + name + '</span>' +
+                '<p class="key">' + url + '</p>' +
+            '</a>'
+        );
+    });
+
     $('#addrLinkGroup').click(function(e) {
         e.stopPropagation();
         $(this).css('opacity', '1');
